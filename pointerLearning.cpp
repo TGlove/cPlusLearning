555917c7f5d64575f25f712c004486966b4637b5
TGlove <1434514070@qq.com>
2019-07-23 15:00:30 +0800

    Create pointerLearning.cpp

---
+++ pointerLearning.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include <iostream>
+#define Max(a,b) ((a>b)?a:b)
+using namespace std;
+//pointer
+void main()
+{
+	/*int  n = 1, *p = 0;
+	p = &n;
+	cout << p << endl;
+	cout << *p << endl;
+	cout << &p << endl;
+	cout << &n << endl;*/
+
+}
